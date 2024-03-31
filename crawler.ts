628e581a60b58be7f9e150b63cc21945902f80a7
QQBoxy <gs1458@gmail.com>
2024-03-31 21:04:02 +0800

    Crawler written using ts

--- crawler.ts
+++ crawler.ts
@@ -1,13 +1,22 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { parse } = require('node-html-parser');
-const fs = require('fs');
-const fsPromises = fs.promises;
+import { promises as fsPromises } from 'fs';
+import { HTMLElement, parse } from 'node-html-parser';
+
+interface DicomTag {
+  name: string;
+  keyword: string;
+  vr: string;
+  vm: string;
+}
+
+interface DicomTags {
+  [Key: string]: DicomTag;
+}
 
 const DICOM_CHAPTER_6 =
   'https://dicom.nema.org/medical/dicom/current/output/chtml/part06/chapter_6.html';
 const DICOM_TAGS_JSON_PATH = './src/assets/libraries/dicomTags.json';
 
-function getText(element) {
+function getText(element: HTMLElement) {
   let str = element.querySelector('span')?.text ?? '';
   if (str === '') {
     str = element.querySelector('p')?.text ?? '';
@@ -22,11 +31,11 @@ async function main() {
   const text = await res.text();
   const root = parse(text);
   const tableDiv = root.querySelector('.table');
-  const table = tableDiv.querySelector('table');
-  const tbody = table.querySelector('tbody');
-  const trList = tbody.querySelectorAll('tr');
-  const dicomTagsJson = {};
-  for (let tr of trList) {
+  const table = tableDiv?.querySelector('table');
+  const tbody = table?.querySelector('tbody');
+  const trList = tbody?.querySelectorAll('tr') ?? [];
+  const dicomTagsJson: DicomTags = {};
+  for (const tr of trList) {
     const tdList = tr.querySelectorAll('td');
     const oringKey = getText(tdList[0]).toUpperCase();
     const name = getText(tdList[1]);
@@ -34,13 +43,11 @@ async function main() {
     const vr = getText(tdList[3]);
     const vm = getText(tdList[4]);
     const match = oringKey.match(/\(([^(,]+),([^,)]+)\)/);
-    const key = `${match[1]}|${match[2]}`;
-    dicomTagsJson[key] = {
-      name,
-      keyword,
-      vr,
-      vm,
-    };
+    if (match) {
+      const key = `${match[1]}|${match[2]}`;
+      const dicomTag: DicomTag = { name, keyword, vr, vm };
+      dicomTagsJson[key] = dicomTag;
+    }
   }
   await fsPromises.writeFile(
     DICOM_TAGS_JSON_PATH,
